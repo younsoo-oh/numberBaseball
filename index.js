af5fda778f9bf25874f887e7a72759a04b9d3565
oys <vkdnjdbstn@gmail.com>
2020-06-30 23:56:43 +0900

    수정

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ for (let i=0; i<=9; i++){
 let selectNumber = [];
 for (let n=0; n<=3; n++){
     const select = Math.floor(Math.random() * list.length); //랜덤으로 4자리뽑기
-    selectNumber.push(list[select]); //선택된 숫자를 넣음  
-    list.splice(select, 1);   //중복되지않도록 넣은거 삭제
+    selectNumber.push(list[select]); //list에서 선택된 숫자를 넣음  
+    list.splice(select, 1);   //중복되지않도록 선택된 숫자 삭제
 }
 console.log(selectNumber); //console에서 정답보여지도록
   
 let count = 1;
 check.addEventListener('click', ()=>{
-    const value = input.value; //문자열
+    const value = input.value; //문자열 상태
     const valueArray = value.split(''); //split은 문자열을 배열로 만들어줌
     if (value.length === 4){
         if (selectNumber.join('') === value) { //join은 배열을 문자열로 만들어줌
@@ -25,10 +25,10 @@ check.addEventListener('click', ()=>{
         } else {
             let strike = 0; //여기서 초기화 하지않으면 값이 쌓임 (ex.strike가 4이상)
             let ball = 0;
-            for (const [s_Keys, s_Number] of selectNumber.entries()){ //entries는 [key,value]를 같이 반환
-                for (const [v_Keys, v_String] of valueArray.entries()){ 
+            for (const [s_index, s_Number] of selectNumber.entries()){ //entries는 [key,value]를 같이 반환
+                for (const [v_index, v_String] of valueArray.entries()){ 
                     if (s_Number === Number(v_String)){ //input값은 숫자가 아닌 문자열이기때문에
-                        if (s_Keys === v_Keys){
+                        if (s_index === v_index){
                             strike++
                         } else{
                             ball++
@@ -43,16 +43,17 @@ check.addEventListener('click', ()=>{
     } else {
         count++
     }
-    if (value.length < 4) {
+    if (value.length !== 4) {
         alert('4자리 숫자를 입력하세요.');
-    } 
-    const findDuplicate = Array.from(new Set(valueArray));
+    } else {
+        const findDuplicate = Array.from(new Set(value));
         if (findDuplicate.length < 4) {
             alert('중복되지 않는 숫자를 입력하세요.');
         }
+    }
     /* 중복되는 숫자를 입력하면 오류가 뜨도록 함 
-    set()은 배열에 넣으면 중복되는 값을 제거 valueArray는 배열상태
-    Array.from()은 객체들을 배열로 변화시킴
+    set()은 중복되는 값을 넘기면 알아서 앞의 값(한개)만 남기고 무시함
+    Array.from()은 배열로 변화시킴
     1) 배열과 비슷한 객체: length 프로퍼티와 인덱스 된 요소를 가지고 있는 객체
     2) 반복할 수 있는 객체 : Map과 Set 객체 및 문자열과 같이 해당 요소를 개별적으로 선택할 수 있는 객체.*/
     input.value = '';
